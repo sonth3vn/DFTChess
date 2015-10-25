1811b4d7bf34ab9618717b0c62cb5ebeb06838b9
sonth3vn <sonth3.vn@gmail.com>
2015-10-25 18:38:48 +0700

    fix Checkmate.cs

--- Assets/Scripts/Models/Checkmate.cs
+++ Assets/Scripts/Models/Checkmate.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class Checkmate : MonoBehaviour {
 	public Point currentPos = new Point();
 
-	void Start(){
-		Rect rect = GetComponent<RectTransform> ().rect;
-		rect = new Rect (rect.position.x, rect.position.y, 100, 100);
-	}
+//	void Start(){
+//		Rect rect = GetComponent<RectTransform> ().rect;
+//		rect = new Rect (rect.position.x, rect.position.y, 100, 100);
+//	}
 
 	public void Pressed(){
 		GameController gc = GameObject.FindObjectOfType<GameController> ();
